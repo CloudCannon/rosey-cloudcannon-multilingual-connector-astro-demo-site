2ad4c0e1fea9bcafb535a5681626b1659febfe55
Tom Richardson <120427204+tomrcc@users.noreply.github.com>
2024-11-13 21:08:47 +1300

    Initial commit

---
+++ rosey/utils/helpers/file-helper.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+async function readFileWithFallback(filepath, fallbackString) {
+  try {
+    const buffer = await fs.promises.readFile(filepath);
+    return buffer.toString("utf-8") || fallbackString;
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      return fallbackString;
+    }
+    throw err;
+  }
+}
+
+async function readJsonFromFile(filepath) {
+  const contents = await readFileWithFallback(filepath, "{}");
+  return JSON.parse(contents);
+}
+
+async function isDirectory(filepath) {
+  const stat = await fs.promises.stat(filepath);
+
+  return stat.isDirectory();
+}
+
+export { readFileWithFallback, readJsonFromFile, isDirectory };
---
+++ src/content/pages/blog.md
@@ -0,0 +1,18 @@
+---
+title: Blog
+page_size: 6
+translations:
+  selected_page_translation: fr-FR
+  translate_es_ES: true
+  translate_de_DE: true
+  translate_fr_FR: true
+seo:
+  page_description: A blog template with tags.
+  canonical_url: null
+  featured_image: null
+  featured_image_alt: null
+  author_twitter_handle: null
+  open_graph_type: website
+  no_index: false
+
+---
