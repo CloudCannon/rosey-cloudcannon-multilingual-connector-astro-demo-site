97dd076199f6a951e3c56ff2e55ac23c7da748d0
tomrcc <tom@cloudcannon.com>
2024-12-13 14:10:40 +1300

    Refactor generateLocales to use config file

--- rosey/utils/generateLocales.js
+++ rosey/utils/generateLocales.js
@@ -2,18 +2,210 @@ import fs from "fs";
 import YAML from "yaml";
 import markdownit from "markdown-it";
 import path from "path";
-import { isDirectory, readFileWithFallback } from "./helpers/file-helper.js";
+import {
+  isDirectory,
+  readFileWithFallback,
+  readConfigFile,
+} from "./helpers/file-helper.js";
 import dotenv from "dotenv";
 const md = markdownit();
 dotenv.config();
 
-const locales = process.env.LOCALES?.split(",");
-const translationsDirPath = "./rosey/translations";
-const localesDirPath = "./rosey/locales";
-const excludedContentFiles = ["config.ts"];
-const contentDirectory = "./src/content";
+(async () => {
+  const configData = await readConfigFile("./rosey/config.yaml");
+  const locales = configData.locales;
+  // Loop through locales
+  for (let i = 0; i < locales.length; i++) {
+    const locale = locales[i];
 
-function getTranslationPath(locale, translationFilename) {
+    try {
+      await generateLocale(locale, configData);
+    } catch (err) {
+      console.error(`❌❌ Encountered an error translating ${locale}:`, err);
+    }
+  }
+})();
+
+// The generateLocales function runs on each separate locale
+async function generateLocale(locale, configData) {
+  console.log(configData);
+  const translationsDirPath = configData.rosey_paths.translations_dir_path;
+  const localesDirPath = configData.rosey_paths.locales_dir_path;
+  const excludedContentFiles = configData.visual_editing.excluded_files;
+  const contentDirectory = configData.visual_editing.content_directory;
+  const baseFile = await fs.promises.readFile(
+    configData.rosey_paths.rosey_base_file_path
+  );
+  const baseFileData = JSON.parse(baseFile.toString("utf-8")).keys;
+  const baseURLsFile = await fs.promises.readFile(
+    configData.rosey_paths.rosey_base_urls_file_path
+  );
+  const baseURLFileData = JSON.parse(baseURLsFile.toString("utf-8")).keys;
+
+  const localePath = path.join(localesDirPath, `${locale}.json`);
+  const localeURLsPath = path.join(localesDirPath, `${locale}.urls.json`);
+  const translationsLocalePath = path.join(translationsDirPath, locale);
+
+  // Ensure directories exist
+  console.log(`📂📂 ${translationsLocalePath} ensuring directory exists`);
+  await fs.promises.mkdir(translationsLocalePath, { recursive: true });
+
+  console.log(`📂📂 ${localesDirPath} ensuring directory exists`);
+  await fs.promises.mkdir(localesDirPath, { recursive: true });
+
+  const oldLocaleData = JSON.parse(
+    await readFileWithFallback(localePath, "{}")
+  );
+  const oldURLsLocaleData = JSON.parse(
+    await readFileWithFallback(localeURLsPath, "{}")
+  );
+
+  const translationsFiles = await fs.promises.readdir(translationsLocalePath, {
+    recursive: true,
+  });
+
+  // - Look through all the pages on the site for translation frontmatter keys
+
+  const contentPagesTranslationConfig = {
+    "404.html": {},
+  };
+  const locales = configData.locales;
+  locales.map((locale) => {
+    const localeKeyName = locale.replace("-", "_");
+    contentPagesTranslationConfig["404.html"][`translate_${localeKeyName}`];
+  });
+  const contentDirectoryFiles = await fs.promises.readdir(contentDirectory, {
+    recursive: true,
+  });
+
+  await Promise.all(
+    contentDirectoryFiles.map(async (file) => {
+      const filePath = path.join(contentDirectory, file);
+      if (
+        !(await isDirectory(filePath)) &&
+        !excludedContentFiles.includes(file)
+      ) {
+        // Check pages frontmatter to see if we should translate
+        const fileRawData = await readFileWithFallback(filePath);
+        const fileFrontmatter = YAML.parse(fileRawData.split("---")[1]);
+        const pageTranslationsConfig = fileFrontmatter.translations;
+        const fileAsHtmlName = file
+          .replace("pages/", "")
+          .replace("index.md", "index.html")
+          .replace(".mdx", "/index.html")
+          .replace(".md", "/index.html");
+        contentPagesTranslationConfig[fileAsHtmlName] = pageTranslationsConfig;
+      }
+    })
+  );
+
+  // Loop through each file in the translations directory
+  const localeDataEntries = {};
+  await Promise.all(
+    translationsFiles.map(async (filename) => {
+      if (
+        await isDirectory(
+          getTranslationPath(locale, translationsDirPath, filename)
+        )
+      ) {
+        return;
+      }
+
+      const response = await processTranslation(
+        locale,
+        filename,
+        translationsDirPath,
+        oldLocaleData,
+        oldURLsLocaleData,
+        baseFileData,
+        baseURLFileData,
+        contentPagesTranslationConfig
+      );
+
+      localeDataEntries[filename] = response;
+    })
+  );
+
+  let localeData = {};
+  let localeURLsData = {};
+  let keysToUpdate = {};
+
+  await Promise.all(
+    Object.keys(localeDataEntries).map(async (filename) => {
+      const { data, urlData } = localeDataEntries[filename];
+
+      Object.keys(urlData).forEach((key) => {
+        localeURLsData[key] = urlData[key];
+      });
+
+      Object.keys(data).forEach((key) => {
+        if (!localeData[key] || data[key].isNewTranslation) {
+          const isKeyStaticOrMarkdown =
+            key.slice(0, 10).includes("static:") ||
+            key.slice(0, 10).includes("markdown:");
+
+          localeData[key] = {
+            original: data[key].original,
+            value:
+              isKeyStaticOrMarkdown && data[key].isNewTranslation
+                ? md.render(data[key].value)
+                : data[key].value,
+          };
+        }
+
+        if (data[key].isNewTranslation) {
+          keysToUpdate[key] = data[key].value;
+        }
+      });
+    })
+  );
+
+  await Promise.all(
+    Object.keys(localeDataEntries).map(async (filename) => {
+      const translationFilePath = getTranslationPath(
+        locale,
+        translationsDirPath,
+        filename
+      );
+      const fileContents = await readFileWithFallback(translationFilePath, "");
+      const data = YAML.parse(fileContents);
+
+      let updatedKeys = [];
+      Object.keys(keysToUpdate).forEach((key) => {
+        if (data[key] || data[key] === "") {
+          data[key] = keysToUpdate[key];
+          updatedKeys = [key];
+        }
+      });
+
+      if (updatedKeys.length > 0) {
+        const yamlString = YAML.stringify(data);
+        await fs.promises.writeFile(translationFilePath, yamlString);
+        console.log(
+          `✅ ${translationFilePath} succesfully updated duplicate keys: ${updatedKeys.join(
+            ", "
+          )}`
+        );
+      }
+    })
+  );
+
+  // Write locales data
+  await fs.promises.writeFile(
+    localePath,
+    JSON.stringify(localeData, null, "\t")
+  );
+  console.log(`✅✅ ${localePath} updated succesfully`);
+
+  // Write locales URL data
+  await fs.promises.writeFile(
+    localeURLsPath,
+    JSON.stringify(localeURLsData, null, "\t")
+  );
+  console.log(`✅✅ ${localeURLsPath} updated succesfully`);
+}
+
+function getTranslationPath(locale, translationsDirPath, translationFilename) {
   return path.join(translationsDirPath, locale, translationFilename);
 }
 
@@ -115,6 +307,7 @@ function processContentTranslationKey(
 async function processTranslation(
   locale,
   translationFilename,
+  translationsDirPath,
   oldLocaleData,
   oldURLsLocaleData,
   baseFileData,
@@ -123,7 +316,11 @@ async function processTranslation(
 ) {
   const localeData = {};
   const localeURLsData = {};
-  const translationsPath = getTranslationPath(locale, translationFilename);
+  const translationsPath = getTranslationPath(
+    locale,
+    translationsDirPath,
+    translationFilename
+  );
   const fileContents = await readFileWithFallback(translationsPath, "");
   const translationHTMLFilename =
     getTranslationHTMLFilename(translationFilename);
@@ -179,177 +376,3 @@ async function processTranslation(
 
   return { data: localeData, urlData: localeURLsData };
 }
-
-// The generateLocales function runs on each separate locale
-async function generateLocale(locale) {
-  const baseFile = await fs.promises.readFile("./rosey/base.json");
-  const baseFileData = JSON.parse(baseFile.toString("utf-8")).keys;
-  const baseURLsFile = await fs.promises.readFile("./rosey/base.urls.json");
-  const baseURLFileData = JSON.parse(baseURLsFile.toString("utf-8")).keys;
-
-  const localePath = path.join(localesDirPath, `${locale}.json`);
-  const localeURLsPath = path.join(localesDirPath, `${locale}.urls.json`);
-  const translationsLocalePath = path.join(translationsDirPath, locale);
-
-  // Ensure directories exist
-  console.log(`📂📂 ${translationsLocalePath} ensuring directory exists`);
-  await fs.promises.mkdir(translationsLocalePath, { recursive: true });
-
-  console.log(`📂📂 ${localesDirPath} ensuring directory exists`);
-  await fs.promises.mkdir(localesDirPath, { recursive: true });
-
-  const oldLocaleData = JSON.parse(
-    await readFileWithFallback(localePath, "{}")
-  );
-  const oldURLsLocaleData = JSON.parse(
-    await readFileWithFallback(localeURLsPath, "{}")
-  );
-
-  const translationsFiles = await fs.promises.readdir(translationsLocalePath, {
-    recursive: true,
-  });
-
-  // - Look through all the pages on the site for translation frontmatter keys
-
-  const contentPagesTranslationConfig = {
-    "404.html": {},
-  };
-  locales.map((locale) => {
-    const localeKeyName = locale.replace("-", "_");
-    contentPagesTranslationConfig["404.html"][`translate_${localeKeyName}`];
-  });
-  const contentDirectoryFiles = await fs.promises.readdir(contentDirectory, {
-    recursive: true,
-  });
-
-  await Promise.all(
-    contentDirectoryFiles.map(async (file) => {
-      const filePath = path.join(contentDirectory, file);
-      if (
-        !(await isDirectory(filePath)) &&
-        !excludedContentFiles.includes(file)
-      ) {
-        // Check pages frontmatter to see if we should translate
-        const fileRawData = await readFileWithFallback(filePath);
-        const fileFrontmatter = YAML.parse(fileRawData.split("---")[1]);
-        const pageTranslationsConfig = fileFrontmatter.translations;
-        const fileAsHtmlName = file
-          .replace("pages/", "")
-          .replace("index.md", "index.html")
-          .replace(".mdx", "/index.html")
-          .replace(".md", "/index.html");
-        contentPagesTranslationConfig[fileAsHtmlName] = pageTranslationsConfig;
-      }
-    })
-  );
-
-  // Loop through each file in the translations directory
-  const localeDataEntries = {};
-  await Promise.all(
-    translationsFiles.map(async (filename) => {
-      if (await isDirectory(getTranslationPath(locale, filename))) {
-        return;
-      }
-
-      // TODO:
-      const response = await processTranslation(
-        locale,
-        filename,
-        oldLocaleData,
-        oldURLsLocaleData,
-        baseFileData,
-        baseURLFileData,
-        contentPagesTranslationConfig
-      );
-
-      localeDataEntries[filename] = response;
-    })
-  );
-
-  let localeData = {};
-  let localeURLsData = {};
-  let keysToUpdate = {};
-
-  await Promise.all(
-    Object.keys(localeDataEntries).map(async (filename) => {
-      const { data, urlData } = localeDataEntries[filename];
-
-      Object.keys(urlData).forEach((key) => {
-        localeURLsData[key] = urlData[key];
-      });
-
-      Object.keys(data).forEach((key) => {
-        if (!localeData[key] || data[key].isNewTranslation) {
-          const isKeyStaticOrMarkdown =
-            key.slice(0, 10).includes("static:") ||
-            key.slice(0, 10).includes("markdown:");
-
-          localeData[key] = {
-            original: data[key].original,
-            value:
-              isKeyStaticOrMarkdown && data[key].isNewTranslation
-                ? md.render(data[key].value)
-                : data[key].value,
-          };
-        }
-
-        if (data[key].isNewTranslation) {
-          keysToUpdate[key] = data[key].value;
-        }
-      });
-    })
-  );
-
-  await Promise.all(
-    Object.keys(localeDataEntries).map(async (filename) => {
-      const translationFilePath = getTranslationPath(locale, filename);
-      const fileContents = await readFileWithFallback(translationFilePath, "");
-      const data = YAML.parse(fileContents);
-
-      let updatedKeys = [];
-      Object.keys(keysToUpdate).forEach((key) => {
-        if (data[key] || data[key] === "") {
-          data[key] = keysToUpdate[key];
-          updatedKeys = [key];
-        }
-      });
-
-      if (updatedKeys.length > 0) {
-        const yamlString = YAML.stringify(data);
-        await fs.promises.writeFile(translationFilePath, yamlString);
-        console.log(
-          `✅ ${translationFilePath} succesfully updated duplicate keys: ${updatedKeys.join(
-            ", "
-          )}`
-        );
-      }
-    })
-  );
-
-  // Write locales data
-  await fs.promises.writeFile(
-    localePath,
-    JSON.stringify(localeData, null, "\t")
-  );
-  console.log(`✅✅ ${localePath} updated succesfully`);
-
-  // Write locales URL data
-  await fs.promises.writeFile(
-    localeURLsPath,
-    JSON.stringify(localeURLsData, null, "\t")
-  );
-  console.log(`✅✅ ${localeURLsPath} updated succesfully`);
-}
-
-(async () => {
-  // Loop through locales
-  for (let i = 0; i < locales.length; i++) {
-    const locale = locales[i];
-
-    try {
-      await generateLocale(locale);
-    } catch (err) {
-      console.error(`❌❌ Encountered an error translating ${locale}:`, err);
-    }
-  }
-})();
